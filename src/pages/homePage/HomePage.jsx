b6a7f35823b32d9434f86cf8ba0c307b1e370a7c
Eva González <evagonzalez.tic@gmail.com>
2023-02-09 19:00:32 +0100

    homepage & api calls

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -3,16 +3,23 @@ import { Container } from "../../components/popUp/PopUp.styles";
 import logo from "../../assets/images/Logo Megablok-54.png";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   //Global state for the welcome message
   const { registered } = useContext(UserContext);
-  //URL
+  //State that validate password
+  const [password, setPassword] = useState(null);
+  //URL wifi
   const wifiNetworksUrl =
     "https://raw.githubusercontent.com/lbernalsierra/FakeAPI/main/Networks.json";
-  //State to store the API call response
+  //URL valid password
+  const validPasswordUrl =
+    "https://raw.githubusercontent.com/lbernalsierra/FakeAPI/main/SetWifi.json";
+  //State to store the API call networks response
   const [wifi, setWifi] = useState([]);
-  //API call
+  //API call showing networks
   useEffect(() => {
     (async () => {
       try {
@@ -20,12 +27,39 @@ const HomePage = () => {
         response = await response.json();
         setWifi(response.networks);
       } catch (error) {
-        console.error(error);
+        console.log(error);
       }
     })();
   }, []);
   //Wifi networks
   const wifiNetwork = wifi.map((item) => item.SSID);
+
+  const navigate = useNavigate(); //Redirect to userPage
+  //Function that validates the password
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    (async () => {
+      try {
+        let response = await fetch(validPasswordUrl);
+        response = await response.json();
+        setPassword(response.valid);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  };
+  //Check when password status changes
+  useEffect(() => {
+    if (password) {
+      Swal.fire({
+        title: "Please,validate your password",
+        input: "text",
+        inputPlaceholder: "Enter your password",
+      });
+      navigate("/user");
+    }
+  }, [password]);
+
   return (
     <>
       <GlobalStyle />
@@ -33,14 +67,14 @@ const HomePage = () => {
         <img src={logo} alt="megablok-logo"></img>
         <h1>Welcome user {registered} </h1>
         <p>Please, select a wifi network and enter the password</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <select>
             {wifiNetwork.map((item, i) => {
               return <option key={i}>{item}</option>;
             })}
           </select>
           <input type="text" placeholder="Key" />
-          <button>Confirm</button>
+          <button type="submit">Confirm</button>
         </form>
       </Container>
     </>
--- src/pages/userPage/userPage.jsx
+++ src/pages/userPage/userPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { GlobalStyle } from "../../components/GlobalStyle.styles";
 
 const userPage = () => {
-  return <div>userPage</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </>
+  );
 };
 
 export default userPage;
